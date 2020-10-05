896fe2d0e4c22025ac1fd5ecc08970c043787b16
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-05 16:20:53 +1100

    Added particles effects on the hompage.

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,56 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: "absolute",
+    opacity: 0.4,
+  },
+});
 
 const Home = () => {
+  const classes = useStyles;
   return (
-    <div>
+    <>
       <Navbar />
       <Header />
-    </div>
+      <Particles
+        canvasClassName={classes.particlesCanvas}
+        params={{
+          particles: {
+            number: {
+              value: 60,
+              density: {
+                enable: true,
+                value_area: 1500,
+              },
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.4,
+            },
+            move: {
+              direction: "random",
+              speed: 1,
+            },
+            size: {
+              value: 1,
+            },
+            opacity: {
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.05,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+    </>
   );
 };
 
