167ca572f5dbf61d1346c0254664236b170ef60a
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-07 14:02:23 +1100

    Imported HashRouter from react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
 import "./App.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Home from "./components";
@@ -7,16 +7,20 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 
-function App() {
-  return (
-    <>
-      <CssBaseline />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+          <CssBaseline />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
