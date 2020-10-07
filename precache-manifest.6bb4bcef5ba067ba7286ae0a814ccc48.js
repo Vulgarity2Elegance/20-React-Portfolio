1e839df430863c54fc3ed72a440b663f3c260b23
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-07 13:33:13 +1100

    Updates

---
+++ precache-manifest.6bb4bcef5ba067ba7286ae0a814ccc48.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6761de63742a9b515d821ec0596877c1",
+    "url": "/20-React-Portfolio/index.html"
+  },
+  {
+    "revision": "13ca5eab30e07f42f536",
+    "url": "/20-React-Portfolio/static/css/main.2808887c.chunk.css"
+  },
+  {
+    "revision": "3102e5eba258a7bdf57f",
+    "url": "/20-React-Portfolio/static/js/2.f8d12978.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/20-React-Portfolio/static/js/2.f8d12978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ca5eab30e07f42f536",
+    "url": "/20-React-Portfolio/static/js/main.f20553f6.chunk.js"
+  },
+  {
+    "revision": "d6002f8051d15f2e320d",
+    "url": "/20-React-Portfolio/static/js/runtime-main.7bf02505.js"
+  },
+  {
+    "revision": "b70bb4dfcab93ada06b2aa9a0a026924",
+    "url": "/20-React-Portfolio/static/media/Weather-Dashboard.b70bb4df.gif"
+  },
+  {
+    "revision": "5fbe4c9f867f96ba3a9596a2962dc83f",
+    "url": "/20-React-Portfolio/static/media/Youzhi Hu - CV.5fbe4c9f.pdf"
+  },
+  {
+    "revision": "71145132ff8082f3168a9cf363781c45",
+    "url": "/20-React-Portfolio/static/media/Youzhi Hu - CV.71145132.docx"
+  },
+  {
+    "revision": "fb7c963acadb40f15f391d4e8ca82473",
+    "url": "/20-React-Portfolio/static/media/avatar.fb7c963a.png"
+  },
+  {
+    "revision": "70fd18ca2e6ebb9a65919bebb552ffe0",
+    "url": "/20-React-Portfolio/static/media/bg-img.70fd18ca.png"
+  },
+  {
+    "revision": "1fef1bbd158512524c40eca0b68cca61",
+    "url": "/20-React-Portfolio/static/media/codeQuiz.1fef1bbd.gif"
+  },
+  {
+    "revision": "990e8d5919d025bef610c36cb87f4bbb",
+    "url": "/20-React-Portfolio/static/media/foodieCrawl.990e8d59.gif"
+  },
+  {
+    "revision": "7a63bcc66d91005a3e01784ab5fad52b",
+    "url": "/20-React-Portfolio/static/media/noteTaker.7a63bcc6.gif"
+  },
+  {
+    "revision": "ccedad0dff5211e6a576c0c85a956f1e",
+    "url": "/20-React-Portfolio/static/media/profile.ccedad0d.png"
+  },
+  {
+    "revision": "372e8cdf1b07ae3b2389af59182ecfac",
+    "url": "/20-React-Portfolio/static/media/quarantineChef.372e8cdf.png"
+  },
+  {
+    "revision": "8c6b379aa038399f06cb31a64a0727eb",
+    "url": "/20-React-Portfolio/static/media/workoutTracker.8c6b379a.png"
+  }
+]);
